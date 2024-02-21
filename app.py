8274166161200859aaecfab56f1c8c85b9e1b3f4
Wenxuan Zhao <xiaodoubizwx@gmail.com>
2024-02-21 17:25:04 -0600

    fix several issues: some experiment do not display plot; change layout of variable explores; add two more variables: cell_type and tissue; fix the color inconsistency: now each cell_line and measure get their distinctive colors

--- app.py
+++ app.py
@@ -7,28 +7,37 @@ from pathlib import Path
 
 grouped_df = pd.read_csv(Path(__file__).parent / "grouped_df.csv")
 grouped_df = grouped_df.rename(columns={grouped_df.columns[0]: 'id'})
-column_names = grouped_df.columns.tolist()[1:6]
+column_names = grouped_df.columns.tolist()[1:8]
 unique_values_dict = {col: grouped_df[col].unique().tolist() for col in column_names}
 experiments_treatment_dict = {name: group["treatment"].tolist() for name, group in grouped_df.groupby("experiment")}
-vars = ["nad(p)h lifetime (tm)", "fad lifetime (tm)", "nad(p)h intensity", "fad intensity", "redox ratio"]
-vars_colnames = ["ntm_mean", "ftm_mean", "nint_mean", "fint_mean", "normrr_mean"]
+vars_colnames = ["na1_mean", "na2_mean","nt1_mean","nt2_mean","ntm_mean", "fa1_mean","fa2_mean","ft1_mean","ft2_mean","ftm_mean", "nint_mean", "fint_mean", "normrr_mean"]
+vars = [name.replace("_mean", "") for name in vars_colnames[:-1]] + ["rr"]
+
+# generate colors for vars and cell lines
+cm = plt.get_cmap('tab20')
+var_colors = {var: cm(i) for i, var in enumerate(vars)}
+cell_line_colors = {cell_line: cm(i) for i, cell_line in enumerate(unique_values_dict[column_names[0]])}
+
 # Define the UI layout
 app_ui = ui.page_navbar(
     ui.nav_panel("1", 
         ui.navset_tab(  
             ui.nav_panel("Data Table", ui.layout_sidebar(
-                ui.panel_sidebar(
-                    ui.layout_columns(ui.input_checkbox_group(column_names[0], "Cell Line",["All"] + unique_values_dict[column_names[0]], selected=["All"]),
-                        ui.input_checkbox_group(column_names[1], "Cancer", ["All"] + unique_values_dict[column_names[1]], selected=["All"]),
+                ui.sidebar(
+                    ui.layout_columns(ui.input_checkbox_group(column_names[0], "Cell Line",["All"], selected=["All"]),
+                        ui.input_checkbox_group(column_names[5], "Cell Type", ["All"] + unique_values_dict[column_names[5]], selected=["All"]), 
+                        ui.input_checkbox_group(column_names[6], "Tissue", ["All"] + unique_values_dict[column_names[6]], selected=["All"]),                       
+                        col_widths=(4, 4, 4)),
+                    ui.layout_columns(ui.input_checkbox_group(column_names[1], "Cancer", ["All"] + unique_values_dict[column_names[1]], selected=["All"]),
                         ui.input_checkbox_group(column_names[2], "Media", ["All"] + unique_values_dict[column_names[2]], selected=["All"]), 
-                        col_widths=(4, 3, 5)),
-                    ui.layout_columns(ui.input_checkbox_group(column_names[3], "Experiment",["All"] + unique_values_dict[column_names[3]], selected=["All"]),
+                        col_widths=(5, 7)),
+                    ui.layout_columns(
+                        ui.input_checkbox_group(column_names[3], "Experiment",["All"] + unique_values_dict[column_names[3]], selected=["All"]),
                         ui.input_checkbox_group(column_names[4], "Treatment", []), 
-                        col_widths=(6,6))
-                ), ui.panel_main(
-                    HTML("<h2>Dataset Explorer</h2>"),
-                    ui.output_data_frame("filtered_table"),
-                ))),
+                        col_widths=(6,6)), width=400, 
+                ), HTML("<h2>Dataset Explorer</h2>"),
+                   ui.output_data_frame("filtered_table"),
+                )),
             ui.nav_panel("Plots", ui.layout_sidebar(
                 ui.panel_sidebar(   
                     ui.layout_columns(
@@ -59,10 +68,22 @@ def server(input, output, session):
     @render.data_frame
     def filtered_table():
         filtered_df = grouped_df
-        for col in column_names[:5]:
+        for col in column_names[:7]:
             if "All" not in input[col]():
                 filtered_df = filtered_df[filtered_df[col].isin(input[col]())]
         return filtered_df
+    
+    @reactive.Effect
+   
+    def _():
+        for col in column_names[:7]:
+            if col == "treatment":
+                continue
+            i = input[col]()
+            if "All" in i:
+                ui.update_checkbox_group(col, choices=["All"], selected=["All"])
+            else:
+                ui.update_checkbox_group(col, choices=["All"] + unique_values_dict[col], selected=i)
     
     @reactive.Effect
     def _():
@@ -132,9 +153,13 @@ def server(input, output, session):
         elif one_cell_line and (all_measures or more_than_one_measures):
             # print("one cell line, more than one measures")
             ui.update_selectize(id="select_cell_line", label="You have chosen 1 cell line. If you want other cell lines, please select only 1 measure.", choices=selected_cell_line, selected=selected_cell_line)
+            if all_measures:
+                ui.update_selectize(id="select_var", label="You have chosen all measures.", choices=selected_measure, selected=selected_measure)
         elif one_measure and (all_cell_lines or more_than_one_cell_line):
             # print("one measure, more than one cell lines")
             ui.update_selectize(id="select_var", label="You have chosen 1 measure. If you want other measures, please select only 1 cell line.", choices=selected_measure, selected=selected_measure)
+            if all_cell_lines:
+                ui.update_selectize(id="select_cell_line", label="You have chosen all cell lines.", choices=selected_cell_line, selected=selected_cell_line)
         else: 
             print("illegal combination")
 
@@ -154,7 +179,7 @@ def server(input, output, session):
             return None
               
         fig, ax = plt.subplots(1,1)
-        ax.set_title("% Change vs Control in " + r"$\bf{" +experiment+ "}$" + " across treatments", fontsize=16)
+        ax.set_title("% Change vs Control in " + r"$\bf{" +experiment+ "}$" + " across treatments", fontsize=15)
         if len(measures) > 1:
             ys = measures
             isY = "measures"
@@ -168,33 +193,40 @@ def server(input, output, session):
                 if isY == "measures":
                     row = grouped_df[(grouped_df["experiment"] == experiment) & (grouped_df["treatment"] == treatment) & (grouped_df["cell_line"] == cell_lines[0])]
                     if len(row) > 1 or len(row) == 0:
-                        return None
-                    
-                    value = float(row[vars_colnames[vars.index(y)]].iloc[0])
-                    error = float(row[vars_colnames[vars.index(y)].replace("_mean", "_se")].iloc[0])
+                        value = np.nan
+                        error = np.nan
+                    else:
+                        value = float(row[vars_colnames[vars.index(y)]].iloc[0])
+                        error = float(row[vars_colnames[vars.index(y)].replace("_mean", "_se")].iloc[0])
                     y_dict[y][0].append(value)
                     y_dict[y][1].append(error)
                 else:
                     row = grouped_df[(grouped_df["experiment"] == experiment) & (grouped_df["treatment"] == treatment) & (grouped_df["cell_line"] == y)]
                     if len(row) > 1 or len(row) == 0:
-                        return None
-                    value = float(row[vars_colnames[vars.index(measures[0])]].iloc[0])
-                    error = float(row[vars_colnames[vars.index(measures[0])].replace("_mean", "_se")].iloc[0])
+                        value = np.nan
+                        error = np.nan
+                    else:
+                        value = float(row[vars_colnames[vars.index(measures[0])]].iloc[0])
+                        error = float(row[vars_colnames[vars.index(measures[0])].replace("_mean", "_se")].iloc[0])
                     y_dict[y][0].append(value)
                     y_dict[y][1].append(error)
         
         # plot the data
         for y_name, (values, errors) in y_dict.items():
             # Plot each measure with error bars
-            plt.errorbar(treatments, values, yerr=errors, fmt='o', label=y_name)
+            if isY == "measures":
+                ax.errorbar(treatments, values, yerr=errors, fmt='o', label=y_name, color=var_colors[y_name])
+            else:
+                ax.errorbar(treatments, values, yerr=errors, fmt='o', label=y_name, color=cell_line_colors[y_name])
+            # ax.errorbar(treatments, values, yerr=errors, fmt='o', label=y_name)
            
         ax.axhline(y=100, color='gray', linestyle='-', label='control')
-        ax.legend(loc='upper left', bbox_to_anchor=(1.05, 1), fontsize=15)
+        ax.legend(loc='upper left', bbox_to_anchor=(1.05, 1), fontsize=12)
         plt.subplots_adjust(right=0.8)
         # Customization
         ax.set_xlabel('Treatments', fontsize=14, fontweight='bold')
         ax.set_ylabel('% change VS control', fontsize=14, fontweight='bold')
-        ax.tick_params(axis='x', labelsize=12)  
+        ax.tick_params(axis='x', labelsize=10)  
         ax.tick_params(axis='y', labelsize=12)  
 
         return fig
